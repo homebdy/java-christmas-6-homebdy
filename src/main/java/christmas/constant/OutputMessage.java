dd8675b0f1879629eb5020b400259058b03fc2ff
김연진 <love_homebody@naver.com>
2023-11-13 18:58:42 +0900

    feat(OutputView): 방문 날짜 안내 문구 출력

--- src/main/java/christmas/constant/OutputMessage.java
+++ src/main/java/christmas/constant/OutputMessage.java
@@ -2,7 +2,8 @@ package christmas.constant;
 
 public enum OutputMessage {
 
-    START_PLANNER("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    START_PLANNER("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."),
+    READ_VISIT_DATE("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
 
     private final String message;
 
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -7,4 +7,8 @@ public class OutputView {
     public void printStartPlanner() {
         System.out.println(OutputMessage.START_PLANNER.getMessage());
     }
+
+    public void printVisitDate() {
+        System.out.println(OutputMessage.READ_VISIT_DATE.getMessage());
+    }
 }
