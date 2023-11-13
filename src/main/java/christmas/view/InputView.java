bad5bf9692820cca7cfa6d9c4af298fe6866ee80
김연진 <love_homebody@naver.com>
2023-11-13 19:22:57 +0900

    feat(InputView): 주문 메뉴 및 개수 입력

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -4,12 +4,24 @@ import camp.nextstep.edu.missionutils.Console;
 import christmas.domain.Date;
 import christmas.validator.InputValidator;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
 
+    private static final String MENU_REGEX = ",";
+
     private final InputValidator validator = new InputValidator();
+
     public Date readDate() {
         String input = Console.readLine();
         validator.validateNumber(input);
         return new Date(Integer.parseInt(input));
+    }
+
+    public List<String> readMenu() {
+        String input = Console.readLine();
+        return Arrays.stream(input.split(MENU_REGEX))
+                .toList();
     }
 }
