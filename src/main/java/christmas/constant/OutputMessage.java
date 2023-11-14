ef92ea1a5045fc6206d5838f10d94e0fadf9d38a
김연진 <love_homebody@naver.com>
2023-11-14 20:30:21 +0900

    feat(WeekdayDiscount): 평일 할인을 받은 경우 해택 내역 출력

--- src/main/java/christmas/constant/OutputMessage.java
+++ src/main/java/christmas/constant/OutputMessage.java
@@ -14,6 +14,7 @@ public enum OutputMessage {
     D_DAY_DISCOUNT("크리스마스 디데이 할인: -"),
     PRESENT_DISCOUNT("증정 이벤트: -"),
     SPECIAL_DISCOUNT("특별 할인: -"),
+    WEEKDAY_DISCOUNT("평일 할인: -"),
     AFTER_DISCOUNT("<할인 후 예상 결제 금액>"),
     EVENT_BADGE("<12월 이벤트 배지>"),
     COUNT("개"),
--- src/main/java/christmas/domain/WeekdayDiscount.java
+++ src/main/java/christmas/domain/WeekdayDiscount.java
@@ -1,20 +1,32 @@
 package christmas.domain;
 
+import christmas.constant.OutputMessage;
+
 public class WeekdayDiscount implements DiscountPolicy {
 
     private static final int DISCOUNT_UNIT = 2023;
     private int discountAmount = 0;
+    private boolean isApply = false;
 
     @Override
     public int getDiscountAmount(EventPlanner eventPlanner) {
         if (eventPlanner.isWeekDay()) {
             discountAmount = eventPlanner.getDessertCount() * DISCOUNT_UNIT;
+            isApply = true;
         }
         return discountAmount;
     }
 
     @Override
-    public int display() {
-        return 0;
+    public boolean isApply() {
+        return isApply;
+    }
+
+    @Override
+    public String display() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(OutputMessage.WEEKDAY_DISCOUNT.getMessage())
+                .append(String.format(OutputMessage.AMOUNT.getMessage(), discountAmount));
+        return stringBuilder.toString();
     }
 }
