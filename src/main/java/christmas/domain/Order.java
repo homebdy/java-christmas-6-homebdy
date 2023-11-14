f9ff923fff79b8117d3692ec2fed66b8ba289d35
김연진 <love_homebody@naver.com>
2023-11-14 23:59:49 +0900

    feat(Order): 주문 형식이 올바르지 않을 경우 예외 발생

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,10 +1,12 @@
 package christmas.domain;
 
+import christmas.constant.ExceptionMessage;
 import christmas.constant.Menu;
 import christmas.constant.OutputMessage;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Order {
 
@@ -21,9 +23,20 @@ public class Order {
     }
 
     private List<String> splitInput(String input) {
-        return Arrays.stream(input.split(DELIMITER))
+        List<String> order = Arrays.stream(input.split(DELIMITER))
                 .toList();
+        validateNumber(order.get(MENU_QUANTITY));
+        return order;
     }
+
+    public void validateNumber(String input) {
+        String NUMBER_REGEXP = "^\\d*$";
+        if(!Pattern.matches(NUMBER_REGEXP, input)) {
+            ExceptionMessage message = ExceptionMessage.INVALID_MENU;
+            throw new IllegalArgumentException(message.getMessage());
+        }
+    }
+
 
     public int getPrice() {
         int price = menu.getPrice();
