2bf058a2c6d0188ca25f86ffc46ed35601da8e5e
김연진 <love_homebody@naver.com>
2023-11-15 02:18:44 +0900

    refactor(InputValidator): 공백 추가

--- src/main/java/christmas/validator/InputValidator.java
+++ src/main/java/christmas/validator/InputValidator.java
@@ -10,14 +10,14 @@ public class InputValidator {
     private static final String DELIMITER_REGEXP = "^([a-zA-Zㄱ-힣]*-[\\d]*,)*([a-zA-Zㄱ-힣]*-[\\d]*)$";
 
     public void validateNumber(String input) {
-        if(!Pattern.matches(NUMBER_REGEXP, input)) {
+        if (!Pattern.matches(NUMBER_REGEXP, input)) {
             ExceptionMessage message = ExceptionMessage.INVALID_DATE;
             throw new IllegalArgumentException(message.getMessage());
         }
     }
 
     public void validateDelimiter(String input) {
-        if(!Pattern.matches(DELIMITER_REGEXP, input)) {
+        if (!Pattern.matches(DELIMITER_REGEXP, input)) {
             ExceptionMessage message = ExceptionMessage.INVALID_MENU;
             throw new IllegalArgumentException(message.getMessage());
         }
