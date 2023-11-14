0730d4bbd1711d5377d828f76253fb463a0e8497
김연진 <love_homebody@naver.com>
2023-11-14 20:35:44 +0900

    feat(WeekendDiscount): 주말 할인을 받은 경우 해택 내역 출력

--- src/main/java/christmas/constant/OutputMessage.java
+++ src/main/java/christmas/constant/OutputMessage.java
@@ -15,6 +15,7 @@ public enum OutputMessage {
     PRESENT_DISCOUNT("증정 이벤트: -"),
     SPECIAL_DISCOUNT("특별 할인: -"),
     WEEKDAY_DISCOUNT("평일 할인: -"),
+    WEEKEND_DISCOUNT("주말 할인: -"),
     AFTER_DISCOUNT("<할인 후 예상 결제 금액>"),
     EVENT_BADGE("<12월 이벤트 배지>"),
     COUNT("개"),
